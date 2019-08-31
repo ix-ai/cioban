40866738d2ce3c6ff75e7e57256bd14675ba6756
Alex Thomae <alex@thom.ae>
2019-08-31 19:20:04 +0000

    Closes #1: rewrite in python
    * deprecates `VERBOSE` and `DISABLE_HEARTBEAT`
    * adds prometheus metrics
    * updated README.md

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Constants declarations """
+
+LOG_DEPRECATION = "Please use LOGLEVEL instead."
+VERBOSE_DEPRECATION = "{} {}".format("Deprecation: VERBOSE has been deprecated.", LOG_DEPRECATION)
+DISABLE_HEARTBEAT_DEPRECATION = "{} {}".format("Deprecation: DISABLE_HEARTBEAT has been deprecated.", LOG_DEPRECATION)
+VERSION = '0.7.0'

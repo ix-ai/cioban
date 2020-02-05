fd91fd3ab4c2100dfcc2f1bf42465e27c363c7ae
Alex Thomae <alex@thom.ae>
2020-02-05 07:48:09 +0100

    Removes all the 'not set' log entries. Adds startup notification

--- cioban/__main__.py
+++ cioban/__main__.py
@@ -23,58 +23,46 @@ if __name__ == '__main__':
         filters = os.environ.get('FILTER_SERVICES')
         filters = filters.split('=', 1)
         options['filters'] = {filters[0]: filters[1]}
-        log.info('FILTER_SERVICES: "{}"'.format(options['filters']))
-    else:
-        log.info('FILTER_SERVICES not set')
+        log.info(f"FILTER_SERVICES: '{options['filters']}'")
 
     if os.environ.get('BLACKLIST_SERVICES'):
         blacklist = os.environ.get('BLACKLIST_SERVICES')
         options['blacklist'] = blacklist.split(' ')
-        log.info('BLACKLIST_SERVICES: "{}"'.format(options['blacklist']))
-    else:
-        log.info('BLACKLIST_SERVICES not set')
+        log.info(f"BLACKLIST_SERVICES: '{options['blacklist']}'")
 
     if os.environ.get('TELEGRAM_TOKEN'):
         options['telegram_token'] = os.environ.get('TELEGRAM_TOKEN')
         log.info('TELEGRAM_TOKEN is set')
-    else:
-        log.info('TELEGRAM_TOKEN not set')
 
     if os.environ.get('TELEGRAM_CHAT_ID'):
         options['telegram_chat_id'] = os.environ.get('TELEGRAM_CHAT_ID')
         log.info('TELEGRAM_CHAT_ID is set')
-    else:
-        log.info('TELEGRAM_CHAT_ID not set')
 
     if os.environ.get('NOTIFY_INCLUDE_NEW_IMAGE'):
         options['notify_include_new_image'] = True
         log.info('NOTIFY_INCLUDE_NEW_IMAGE is set')
-    else:
-        log.info('NOTIFY_INCLUDE_NEW_IMAGE not set')
 
     if os.environ.get('NOTIFY_INCLUDE_OLD_IMAGE'):
         options['notify_include_old_image'] = True
         log.info('NOTIFY_INCLUDE_OLD_IMAGE is set')
-    else:
-        log.info('NOTIFY_INCLUDE_OLD_IMAGE not set')
 
     if options.get('telegram_token') and options.get('telegram_chat_id'):
         options['notifiers'].append('telegram')
 
     options['sleep_time'] = os.environ.get('SLEEP_TIME', '5m')
-    log.info('SLEEP_TIME: {}'.format(options['sleep_time']))
+    log.info(f"SLEEP_TIME: {options['sleep_time']}")
 
     options['prometheus_port'] = int(os.environ.get('PORT', 9308))
-    log.info('PORT: {}'.format(options['prometheus_port']))
+    log.info(f"PORT: {options['prometheus_port']}")
 
-    log.warning(
-        "Starting {} {}-{} with prometheus metrics on port {}".format(
-            __package__,
-            constants.VERSION,
-            constants.BUILD,
-            options['prometheus_port'],
-        )
+    startup_message = "Starting {} {}-{} with prometheus metrics on port {}".format(
+        __package__,
+        constants.VERSION,
+        constants.BUILD,
+        options['prometheus_port'],
     )
+    log.warning(startup_message)
 
     c = cioban.Cioban(**options)
+    c.notify(title="CIOBAN Startup", message=startup_message)
     c.run()

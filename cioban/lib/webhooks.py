b0f3782a1ba62707c422ceb6b0c9b335f3024f79
Alex Thomae <alex@thom.ae>
2020-03-01 19:17:47 +0100

    Adds the webhook trigger (#6)

--- cioban/lib/webhooks.py
+++ cioban/lib/webhooks.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 """ Handles webhooks """
 import logging
-from docker.client import DockerClient
 from urllib.parse import urlparse
+import requests
+from docker.client import DockerClient
+from . import constants
+from .helpers import short_msg
 
 log = logging.getLogger('cioban')
 
@@ -25,14 +28,14 @@ class Webhooks():
         'auth.basic.username': {
             'default': None,
         },
-        'auth.basic.passwork': {
+        'auth.basic.password': {
             'default': None,
         },
         'auth.token.header': {
-            'default': None,
+            'default': 'Authorization',
         },
         'auth.token.type': {
-            'default': None,
+            'default': 'token',
         },
         'auth.token.token': {
             'default': None,
@@ -51,8 +54,9 @@ class Webhooks():
             value = service.attrs['Spec']['Labels'].get(f"cioban.webhook.{label}")
             if value:
                 self._update_label(label, value)
-        if self.active:
-            log.debug(f'{self.service.name}: Webhook registered')
+        if self.labels.get('http.url'):
+            self.active = True
+            log.debug(f'{self.service.name}: Webhook active')
 
     def validate_url(self, url: str) -> bool:
         """ Validates if a string is a valid URL """
@@ -78,19 +82,48 @@ class Webhooks():
                     )
                     value = label_definition['default']
 
-            log.debug(f"{self.service.name}: Registering {label}: {value}")
-            result = {label: value}
-            self.labels.update(result)
+            if label == 'http.url' and not self.validate_url(value):
+                log.warning(f"{self.service.name}: Value '{value}' for label {label} is invalid")
+            else:
+                log.debug(f"{self.service.name}: Using '{value}' for {label}")
+                result = {label: value}
+                self.labels.update(result)
 
         return result
 
     def trigger(self):
         """ Triggers the webhook """
         if not self.active:
-            log.debug("No webhook configured")
+            log.debug(f"{self.service.name}: Webhook not configured")
             return
 
         url = self.labels['http.url']
         method = self.labels.get('http.method', self.describe_labels['http.method']['default'])
         timeout = int(self.labels.get('http.timeout', self.describe_labels['http.timeout']['default']))
-        log.debug(f"{self.service.name}: {url} {method} {timeout}")
+        auth = None
+        headers = {
+            'User-Agent': f'cioban {constants.VERSION}-{constants.BUILD}'
+        }
+
+        if self.labels.get('auth.basic.username') and self.labels.get('auth.basic.password'):
+            auth = (self.labels['auth.basic.username'], self.labels['auth.basic.password'])
+            if self.labels.get('auth.token.token') and (
+                    not self.labels.get('auth.token.header')
+                    or self.labels.get('auth.token.header') == 'Authorization'
+            ):
+                log.warning(f"{self.service.name}: {constants.W001} (W001)")
+
+        if self.labels.get('auth.token.token'):
+            auth_name = self.labels.get('auth.token.header', self.describe_labels['auth.token.header']['default'])
+            auth_value = self.labels.get('auth.token.type', self.describe_labels['auth.token.type']['default'])
+            headers.update({
+                auth_name: f"{auth_value} {self.labels['auth.token.token']}"
+            })
+        action = getattr(requests, method)
+
+        try:
+            response = action(url, auth=auth, headers=headers, timeout=timeout)
+            log.debug(f"{self.service.name}: Webhook response: {response.content}")
+        except requests.exceptions.RequestException as e:  # this catches all exceptions from requests
+            log.warning(f"{self.service.name}: Could not trigger webhook. The Exception: {short_msg(e)}")
+            log.debug(f"{self.service.name}: The Exception: {e}")

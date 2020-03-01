e6017c0beb09fffe65afff2a4c399ebdfdd56568
Alex Thomae <alex@thom.ae>
2020-03-01 18:37:15 +0100

    Adds W001 message (#6)

--- cioban/lib/constants.py
+++ cioban/lib/constants.py
@@ -4,3 +4,10 @@
 
 VERSION = None
 BUILD = None
+
+# Messages
+
+W001 = (
+        'Not using cioban.webhook.auth.token since both cioban.webhook.auth.basic and cioban.webhook.auth.token are'
+        ' set. Either coose a different value for cioban.webhook.auth.token.header or don\'t use cioban.auth.basic.'
+)
